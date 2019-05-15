21aba7cf5f5324ded4377c4100072aacb1acf8f7
MeItsLars <larsjeurissen@hotmail.nl>
2019-05-15 15:44:39 +0200

    Database test

--- app/src/main/java/ru/group12/tinytasks/MainActivity.java
+++ app/src/main/java/ru/group12/tinytasks/MainActivity.java
@@ -1,7 +1,19 @@
 package ru.group12.tinytasks;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +22,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Hoi Lien
-        //haiiiii
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference ref = database.getReference("Users").child("1");
+
+        Toast.makeText(MainActivity.this, "Updated.", Toast.LENGTH_LONG);
+
+        final TextView currentValue = findViewById(R.id.dbTextView);
+        final Button updateButton = findViewById(R.id.updateButton);
+        final EditText newValue = findViewById(R.id.textValue);
+
+        updateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ref.setValue(newValue.getText().toString());
+            }
+        });
+
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                currentValue.setText(dataSnapshot.getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
