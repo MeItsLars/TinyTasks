e3c7f0224b6d893b3f1b4ff822f48d55c2d0a7b7
Lien Wullink <p3Rd1#dU^b>
2019-06-04 12:59:01 +0200

    In activity_home_screen.xml heb ik activity_joboverview.xml veranderd naar activity_taskoverview.xml
    Homescreen laten voor wat het is, totdat er een code voor is bedacht. Layout daar op aan passen.

--- app/src/main/java/ru/group12/tinytasks/joboverview.java
+++ app/src/main/java/ru/group12/tinytasks/joboverview.java
@@ -8,6 +8,6 @@ public class joboverview extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_joboverview);
+        setContentView(R.layout.activity_taskoverview);
     }
 }
