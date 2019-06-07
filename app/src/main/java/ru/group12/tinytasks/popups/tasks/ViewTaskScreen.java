bffd52e3fe51570640262966d2c4d052c47089e4
digitamz <(digitamz@gmail.com)>
2019-06-07 08:53:58 +0200

    Reactieknop disabled nu na 1 klik op spamclick te voorkomen, verandert de tekst van de button

--- app/src/main/java/ru/group12/tinytasks/popups/tasks/ViewTaskScreen.java
+++ app/src/main/java/ru/group12/tinytasks/popups/tasks/ViewTaskScreen.java
@@ -1,5 +1,6 @@
 package ru.group12.tinytasks.popups.tasks;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -8,6 +9,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import ru.group12.tinytasks.R;
+import ru.group12.tinytasks.popups.signin.SignInScreen;
+import ru.group12.tinytasks.util.ActivityManager;
 import ru.group12.tinytasks.util.TaskManager;
 import ru.group12.tinytasks.util.database.Database;
 import ru.group12.tinytasks.util.database.ImageManager;
@@ -47,7 +50,7 @@ public class ViewTaskScreen extends AppCompatActivity {
         ImageManager.loadTaskImage(this, task, taskImage);
 
         Button backToHome = findViewById(R.id.back_to_home_button);
-        Button reactToTask = findViewById(R.id.react_to_task_button);
+        final Button reactToTask = findViewById(R.id.react_to_task_button);
 
         backToHome.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -56,12 +59,24 @@ public class ViewTaskScreen extends AppCompatActivity {
             }
         });
 
-        reactToTask.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                User sender = Database.getCurrentUser();
-                if(sender != null) NotificationsManager.sendRespondNotification(sender.getUid(), sender.getName(), task.getUserID(), task.getUniqueTaskID());
-            }
-        });
+        if(Database.userSignedIn()) {
+            reactToTask.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    User sender = Database.getCurrentUser();
+                    if(sender != null) NotificationsManager.sendRespondNotification(sender.getUid(), sender.getName(), task.getUserID(), task.getUniqueTaskID());
+                    reactToTask.setText("Reaction sent");
+                }
+            });
+        } else {
+            reactToTask.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                        ActivityManager.startNewActivity(ViewTaskScreen.this, SignInScreen.class, Intent.FLAG_ACTIVITY_NEW_TASK);
+                        reactToTask.setEnabled(false);
+                    }
+
+            });
+        }
     }
 }
