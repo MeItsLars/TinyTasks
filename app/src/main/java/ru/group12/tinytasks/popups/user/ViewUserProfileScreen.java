16a6a19dd1b5d0fcbefbe5908dabac79f1bc91cf
31631 <digitamz@gmail.com>
2019-06-07 09:45:24 +0200

    Copy email en nr to clipboard

--- app/src/main/java/ru/group12/tinytasks/popups/user/ViewUserProfileScreen.java
+++ app/src/main/java/ru/group12/tinytasks/popups/user/ViewUserProfileScreen.java
@@ -1,8 +1,13 @@
 package ru.group12.tinytasks.popups.user;
 
+import android.content.ClipData;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.content.ClipboardManager;
 
 import ru.group12.tinytasks.R;
 import ru.group12.tinytasks.util.database.objects.User;
@@ -35,5 +40,24 @@ public class ViewUserProfileScreen extends AppCompatActivity {
         email.setText("Email: " + user.getEmail());
         phoneNumber.setText("Phone number: " + user.getPhoneNumber());
         gender.setText("Gender: " + user.getGender());
+
+        Button copyNumber = findViewById(R.id.getPhoneButton);
+        Button copyMail = findViewById(R.id.getMailButton);
+        copyNumber.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                ClipData clip = ClipData.newPlainText("copied to clipboard", user.getPhoneNumber());
+                clipboard.setPrimaryClip(clip);
+            }
+        });
+        copyNumber.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                ClipData clip = ClipData.newPlainText("copied to clipboard", user.getEmail());
+                clipboard.setPrimaryClip(clip);
+            }
+        });
     }
 }
